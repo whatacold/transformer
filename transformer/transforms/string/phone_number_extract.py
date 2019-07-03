a8c591afb040655c399a42d3a0f8f6108330670a
whatacold <whatacold@gmail.com>
2019-07-03 23:44:56 +0800

    Rename variable name URL_REGEX to PHONE_NUMBER_REGEX per context.

--- transformer/transforms/string/phone_number_extract.py
+++ transformer/transforms/string/phone_number_extract.py
@@ -5,7 +5,7 @@ from transformer.transforms.base import BaseTransform
 
 # Regex based on second script in question here (https://stackoverflow.com/questions/3868753/find-phone-numbers-in-python-script)
 # Tweaked to accept international phone numbers, including 2 digit country codes and area codes, along with area codes that begin with a 0: https://gist.github.com/maguay/f3a46f578568a608413530e27b78af88
-URL_REGEX = r"""(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?"""
+PHONE_NUMBER_REGEX = r"""(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?"""
 
 
 class StringPhoneExtractTransform(BaseTransform):
@@ -20,7 +20,7 @@ class StringPhoneExtractTransform(BaseTransform):
 
     def transform(self, str_input, **kwargs):
         if isinstance(str_input, basestring):
-            match = re.search(URL_REGEX, str_input)
+            match = re.search(PHONE_NUMBER_REGEX, str_input)
             return match.group(0) if match else u''
         else:
             return u''
